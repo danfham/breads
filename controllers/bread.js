df321205dd0554e96e499b1ea36f9bb73e4568db
danfham <dan.cunningham91@icloud.com>
2023-07-08 13:14:11 -0400

    added index and views

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,10 +1,12 @@
 const router = require('express').Router()
 const Bread = require('../models/bread')
 
+//this renders the data to the screen
 router.get('/', (req,res) => {
-    res.send(Bread)
+    res.render('index', {breads: Bread})
 })
 
+//this ties back the url on local host to the bread data and sends the data model information
 router.get('/:index',(req,res) => {
     const { index } = req.params
     res.send(Bread[index])
---
+++ views/index.jsx
@@ -0,0 +1,26 @@
+const React = require('react')
+const Default = require('./layouts/Default')
+
+function Index ({breads})  {
+    const breadDisplay = breads.map((bread,i) =>{
+        return(
+            <li key={i}>
+                <a href={`/breads/${i}`}>
+                    {bread.name}
+                </a>
+            </li>
+        )
+    })
+
+    return (
+      <Default>
+        <h2>Index Page</h2>
+        <ul>
+            {breadDisplay}
+        </ul>
+      </Default>
+    )
+}
+
+
+module.exports = Index
